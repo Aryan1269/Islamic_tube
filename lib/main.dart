1e7b1049562313d60da73cab7c17592e0d33968d
Aryan <aryansarkar81@gmail.com>
2023-02-26 22:48:46 +0530

    fixedSplash

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:islamic_tube/screens/GetStarted.dart';
@@ -7,16 +6,13 @@ import 'package:islamic_tube/splashscreen.dart';
 import 'package:islamic_tube/screens/sigin.dart';
 import 'package:islamic_tube/tabs/bottomNav.dart';
 
-import 'home.dart';
-
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(Merged());
 }
 
 class Merged extends StatefulWidget {
-  
   const Merged({Key? key}) : super(key: key);
 
   @override
@@ -24,41 +20,22 @@ class Merged extends StatefulWidget {
 }
 
 class _MergedState extends State<Merged> {
-  var auth = FirebaseAuth.instance;
-  var islogin=false;
-  checkIfLogin() async{
-    auth.authStateChanges().listen((User? user) {
-      if(user !=null && mounted){
-        setState(() {
-          islogin=true;
-        });
-      }
-     });
-  }
-
-  @override
-  void initState() {
-    checkIfLogin();
-    super.initState();
-  }
   @override
   Widget build(BuildContext context) {
-
-   return MaterialApp(
-    theme: ThemeData(
-      brightness: Brightness.light,
-    ),
-    debugShowCheckedModeBanner: false,
-   routes: {
-        "/": (context) => islogin? const bottomNav():SplashScreen(),
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+      ),
+      debugShowCheckedModeBanner: false,
+      routes: {
+        "/": (context) => SplashScreen(),
         "/getstarted": (context) => const GetStarted(),
         "/sign": (context) => const signin(),
         // "/register": (context) => const register(),
-        "/register": (context) =>const register(),
+        "/register": (context) => const register(),
         "/home": (context) => const bottomNav(),
         // "/search": (context) =>  MysearchDelegate(),
       },
-  );
-    
+    );
   }
 }
--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:islamic_tube/screens/GetStarted.dart';
+import 'package:islamic_tube/tabs/bottomNav.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -10,40 +12,52 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  var auth = FirebaseAuth.instance;
+  var islogin = false;
+  checkIfLogin() async {
+    auth.authStateChanges().listen((User? user) {
+      if (user != null && mounted) {
+        setState(() {
+          islogin = true;
+        });
+      }
+    });
+  }
+
   @override
   void initState() {
+    
     // TODO: implement initState
     super.initState();
     Timer(Duration(seconds: 3), () {
+      checkIfLogin();
       Navigator.of(context)
-          .pushReplacement(MaterialPageRoute(builder: (_) => GetStarted()));
+              .pushReplacement(MaterialPageRoute(builder: (_) =>islogin
+          ? const bottomNav()
+          : GetStarted()));
     });
   }
 
   Widget build(BuildContext context) {
     return Stack(
-        children: [
-          Container(
-            
-            decoration: BoxDecoration(
-            image: DecorationImage(image: AssetImage('assets/splash.png'),fit: BoxFit.fill)
-            ),
-            child: Align(
-              alignment: Alignment.center,
-              child: Image.asset(
-                'assets/IslamicV.png',scale: 5       
-              ),
-            ),
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/splash.png'), fit: BoxFit.fill)),
+          child: Align(
+            alignment: Alignment.center,
+            child: Image.asset('assets/IslamicV.png', scale: 5),
           ),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: Image.asset('assets/image.png'),
-            ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Image.asset('assets/image.png'),
           ),
-        ],
-      
+        ),
+      ],
     );
   }
 }
