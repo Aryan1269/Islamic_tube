6ee267337d6f50485738fc3e8406b75f64cae92e
Aryan <aryansarkar81@gmail.com>
2023-03-15 16:14:44 +0530

    creating

---
+++ lib/bottomSheet/create.dart
@@ -0,0 +1,28 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Create extends StatefulWidget {
+  const Create({Key? key}) : super(key: key);
+
+  @override
+  State<Create> createState() => _CreateState();
+}
+
+class _CreateState extends State<Create> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
